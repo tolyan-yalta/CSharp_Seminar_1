8ef83de769e69660d332bef638d78ee313f3615a
anatoly <tolyan.yalta@yandex.ru>
2022-08-19 22:10:10 +0300

    Create file Task_8

---
+++ Task_8/Program.cs
@@ -0,0 +1,18 @@
+﻿//Программа, которая на вход принимает число (N),
+//а на выходе показывает все чётные числа от 1 до N
+int N; int number;
+number = 1;
+Console.WriteLine("Введите число: ");
+N = Convert.ToInt32(Console.ReadLine());
+while(number < N)
+{
+    if(number % 2 == 0)
+    {
+        Console.Write(number + ", ");
+    }
+    number++;
+}
+if(number % 2 == 0)
+    {
+        Console.Write(number);
+    }
