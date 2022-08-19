840c7be3c3a25131a9331ec3b49ea1c13ab8b468
anatoly <tolyan.yalta@yandex.ru>
2022-08-19 11:52:30 +0300

    Create file Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,20 @@
+﻿//Программа, которая на вход принимает два числа и выдаёт,
+//какое число большее, а какое меньшее
+double a;
+double b;
+string? input1;
+string? input2;
+Console.WriteLine("Введите число a");
+input1 = Console.ReadLine();
+a = Double.Parse(input1);
+Console.WriteLine("Введите число b");
+input2 = Console.ReadLine();
+b = Double.Parse(input2);
+if(a > b)
+{
+    Console.WriteLine("max = " + a);
+}
+else
+{
+    Console.WriteLine("max = " + b);
+}
