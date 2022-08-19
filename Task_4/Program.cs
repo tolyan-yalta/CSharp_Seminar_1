3cefee12871f5777aed3c512af36cc71f8b65347
anatoly <tolyan.yalta@yandex.ru>
2022-08-19 12:10:34 +0300

    Create file Task_4

---
+++ Task_4/Program.cs
@@ -0,0 +1,38 @@
+﻿//Программа, которая принимает на вход три числа
+//и выдаёт максимальное из этих чисел
+double a; double b; double c;
+string? input1;
+string? input2;
+string? input3;
+Console.WriteLine("Введите число a");
+input1 = Console.ReadLine();
+a = Double.Parse(input1);
+Console.WriteLine("Введите число b");
+input2 = Console.ReadLine();
+b = Double.Parse(input2);
+Console.WriteLine("Введите число c");
+input3 = Console.ReadLine();
+c = Double.Parse(input3);
+if(a > b)
+{
+    if(a > c)
+    {
+        Console.WriteLine("Максимальное число " + a);
+    }
+    else
+    {
+        Console.WriteLine("Максимальное число " + c);
+    }
+}
+else
+{
+    if(b > c)
+    {
+        Console.WriteLine("Максимальное число " + b);
+    }
+    else
+    {
+        Console.WriteLine("Максимальное число " + c);
+    }
+}
+
